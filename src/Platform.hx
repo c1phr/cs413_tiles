68dd2d2ddb0a83cb4596fd1678f7f5db4bdadcdf
Nancy McCollough <nm389@nau.edu>
2015-03-30 16:24:02 -0700

    fixed platform dimensions, new platforms added
    I randomized the platform that's chosen to display. I was trying to
    lessen the probability that the really busy platform would get chosen.
    Might need an upgrade... it doesn't look great.

--- src/Platform.hx
+++ src/Platform.hx
@@ -1,5 +1,6 @@
 import starling.display.Sprite;
 import starling.display.Image;
+import Math.*;
 
 class Platform extends Sprite
 {
@@ -8,8 +9,21 @@ class Platform extends Sprite
 	public function new(x:Int, y:Int, textureIndex:Int)
 	{
 		super();
-		platformTextures = ["platform1"];
-		texture = new Image(Root.assets.getTexture(platformTextures[textureIndex-1]));		
+		var num = Std.random(10);
+		if (num == 1) {
+			platformTextures = ["platform2"];
+		}
+		else if (num == 2 || num == 3) {
+			platformTextures = ["platform4"];
+		}
+		else if (num == 4) {
+			platformTextures = ["platform1"];
+		}
+		else {
+			platformTextures = ["platform3"];
+		}
+		texture = new Image(Root.assets.getTexture(platformTextures[textureIndex - 1]));
+		texture.smoothing = "none";
 		this.x = x;
 		this.y = y;
 		this.addChild(texture);
--- src/Root.hx
+++ src/Root.hx
@@ -20,9 +20,7 @@ class Root extends Sprite {
     	assets.enqueue("assets/lizard.png", "assets/baby1.png");
 		assets.enqueue("assets/grass.png");
 		assets.enqueue("assets/white-arrow.png");
-		assets.enqueue("assets/dummy-item.png");
-		assets.enqueue("assets/ground-temp.png");
-		assets.enqueue("assets/platform1.png");
+		assets.enqueue("assets/platform1.png", "assets/platform2.png", "assets/platform3.png", "assets/platform4.png");
 		//assets.enqueue("assets/map1.tmx");
 		assets.enqueue("assets/blanksky.png", "assets/treemiddle.png", "assets/treemiddle2.png", "assets/treemiddle3.png");
 		
