2adb52235230e9b8bd57e082fb7de803f9ad124a
Ryan Batchelder <rb688@nau.edu>
2015-03-30 18:48:15 -0700

    Quick levels comment to describe spikes

--- src/Levels.hx
+++ src/Levels.hx
@@ -1,6 +1,8 @@
 class Levels
 {
 	// Each level should be a 10x10 2D array stored as a static var
+	// Regular platforms are 1-4
+	// Multiply the platform int by 4 to get that platform texture with spikes (so 5-9)
 	static public var Level0:Array<Array<Int>> =
 			 [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
 			  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
